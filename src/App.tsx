aff872fd439f6203b21778da96710342719374f4
Ivan Petrovic <ivan.petrovic@ingsoftware.com>
2022-08-02 09:38:53 +0200

    add h1

--- src/App.tsx
+++ src/App.tsx
@@ -9,6 +9,7 @@ const App = () => {
   return (
     <>
       <div className='app grid-center'>
+        <h1>Interactive comments section</h1>
         <div className='comments flex-center'>
           {comments.map((comment) => (
             <Comment key={comment.id} comment={comment} />
